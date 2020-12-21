8bba32a794a3f5fbefa966f332fa5391c82d63c6
FrensysThomas87 <delta873@gmail.com>
2020-12-21 15:12:14 +0100

    Impostato calcolo su span

--- js/script.js
+++ js/script.js
@@ -6,18 +6,19 @@ var somma = 0;
 
 document.getElementsByTagName('button')[0].addEventListener("click", function(){
 
-var totalPrice = document.getElementById('total-container');
+var totalPrice = document.getElementById('total-container').getElementsByTagName('span')[0];
 
+//Ad ogni click del pulsante riporto la somma a zero
 somma = 0;
 
+console.log(somma);
 for(var i = 0; i < checkboxes.length; i++){
+  //Qui faccio in modo che la somma venga ricalcolata in base agli elementi chekkati
   if(checkboxes[i].checked){
     somma += parseInt(checkboxes[i].value);
   }
 }
-alert('la somma è ' + somma);
-
-
+totalPrice.innerText = '$ ' + somma;
 
 }
 
