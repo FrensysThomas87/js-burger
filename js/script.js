7262b524a04f9d552355d937d8c11d09c126996c
FrensysThomas87 <delta873@gmail.com>
2020-12-21 15:07:17 +0100

    ottenuto calcolo corretto della somma

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 var checkboxes = document.getElementById('checkbox-container').getElementsByTagName('input');
 
-console.log(checkboxes);
+// console.log(checkboxes);
+
+var somma = 0;
+
+document.getElementsByTagName('button')[0].addEventListener("click", function(){
+
+var totalPrice = document.getElementById('total-container');
+
+somma = 0;
+
+for(var i = 0; i < checkboxes.length; i++){
+  if(checkboxes[i].checked){
+    somma += parseInt(checkboxes[i].value);
+  }
+}
+alert('la somma è ' + somma);
+
+
+
+}
+
+
+
+
+);
