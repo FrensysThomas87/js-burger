b748b2e151a0530f127abb105a57be2b3031ae6f
FrensysThomas87 <delta873@gmail.com>
2020-12-21 15:53:46 +0100

    Ultime modifiche

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var checkboxes = document.getElementById('checkbox-container').getElementsByTagName('input');
+console.log(checkboxes);
 
 // console.log(checkboxes);
 
@@ -6,23 +7,34 @@ var somma = 0;
 
 document.getElementsByTagName('button')[0].addEventListener("click", function(){
 
+
 var totalPrice = document.getElementById('total-container').getElementsByTagName('span')[0];
 
 //Ad ogni click del pulsante riporto la somma a zero
 somma = 0;
 
-console.log(somma);
+// console.log(somma);
+
+
+var checkedElements = 0;
+
 for(var i = 0; i < checkboxes.length; i++){
-  //Qui faccio in modo che la somma venga ricalcolata in base agli elementi chekkati
+
   if(checkboxes[i].checked){
-    somma += parseInt(checkboxes[i].value);
-  }
+    checkedElements++;
 }
-totalPrice.innerText = '$ ' + somma;
-
+//Qui faccio in modo che la somma venga ricalcolata in base agli elementi chekkati
+  // if(checkboxes[i].checked){
+  //   somma += parseInt(checkboxes[i].value);
+  // }
+}
+if(checkedElements < 2){
+  alert('Devi selezionare 2 o più elementi')
+}else{
+  somma += parseInt(checkboxes[i].value);
+  totalPrice.innerText = '$ ' + somma;
 }
 
-
-
+}
 
 );
