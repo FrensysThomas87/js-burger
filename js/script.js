f2d46ba2737c55bb964b00499662ed9b755281c7
FrensysThomas87 <delta873@gmail.com>
2020-12-21 17:21:22 +0100

    Pulizia codice aggiunta dei commenti e fine implementazione funzioni javascript

--- js/script.js
+++ js/script.js
@@ -1,28 +1,27 @@
 var checkboxes = document.getElementById('checkbox-container').getElementsByTagName('input');
 var burgerName = document.getElementsByTagName('input')[0];
 var coupon = document.getElementsByTagName('input')[7];
+var totalPrice = document.getElementById('total-container').getElementsByTagName('span')[0];
+
 var sommaScontata = 0;
-
-
 var somma = 0;
 
+//Variabile che mi che mi controlla quanti elementi sono stati selezionati
+var checkedElementsCounter = 0;
+
+//Lista coupon sconto
 var couponList = ['123ABCDE', '456FGHI', '890BVCDF'];
 
-var discount = false;
 
-
-
-//Inizio funzione click per pulsante
+//INIZIO FUNZIONE CLICK PULSANTE
 document.getElementsByTagName('button')[0].addEventListener("click", function(){
-burgerName = document.getElementsByTagName('input')[0].value;
 
-var totalPrice = document.getElementById('total-container').getElementsByTagName('span')[0];
+//Recupero il valore di burgerName quando clicco sul pulsante
+burgerName = document.getElementsByTagName('input')[0].value;
 
 //Ad ogni click del pulsante riporto la somma a zero
 somma = 0;
-// sommaScontata = 0;
-//Variabile che mi che mi controlla quanti elementi sono stati selezionati
-var checkedElementsCounter = 0;
+
 
 //Ciclo all'interno delle checkboxes
 for(var i = 0; i < checkboxes.length; i++){
@@ -34,11 +33,13 @@ for(var i = 0; i < checkboxes.length; i++){
 }
 
 }
-//Se il contatore è minore di due o se non è stato specificato il nome dell'hamburger
+//Se il contatore è minore di due o se non è stato specificato il nome dell'hamburger manda un alert
 if(checkedElementsCounter < 2 || burgerName == '' ){
   alert('Devi selezionare 2 o più elementi e scrivere il nome dell\'hamburger')
+
+  // Altrimenti scrivi la somma da spendere
 }else{
-  totalPrice.innerText = '$ ' + somma;
+  totalPrice.innerText = '$' + somma;
 
   //Recupero la lista dei coupon sconto
   for(var x = 0; x < couponList.length; x++){
@@ -46,16 +47,13 @@ if(checkedElementsCounter < 2 || burgerName == '' ){
     //Se il codice coupon corrisponde con la lista allora applico il 20% di sconto
     if(coupon.value === couponList[x]){
     sommaScontata = somma - (somma / 100) * 20;
-    totalPrice.innerText = '$ ' + sommaScontata + ' con 20% di sconto !!!!';
+    totalPrice.innerText = '$' + sommaScontata + ' con 20% di sconto !!!!';
     }
 
 
   }
 }
 
-
-
 }
-//Fine funzione click per pulsante
 
-);
+); //FINE FUNZIONE CLICK PULSANTE
